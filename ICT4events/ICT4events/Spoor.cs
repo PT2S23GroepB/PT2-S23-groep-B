5f166d80a86f0dbf61c1950e61239982ed7144b3
DoomTemplarKnight <TempelarofCoD@gmail.com>
2014-10-14 12:12:38 +0200

    Test

--- ICT4events/ICT4events/Spoor.cs
+++ ICT4events/ICT4events/Spoor.cs
@@ -7,5 +7,34 @@ namespace ICT4events
 {
     class Spoor
     {
+        private bool bezet;
+        private List<Sector> sectoren;
+        public bool Bezet { get { return bezet; } }
+
+        private int spoorNr;
+
+        public int SpoorNr { get { return spoorNr; } }
+
+        private string spoorstatus;
+        public string SpoorStatus { get { return spoorstatus; } }
+
+        public Spoor(int spoorNr, int aantalsectoren)
+        {
+            this.spoorNr = spoorNr;
+            int i = 1;
+            sectoren = new List<Sector>();
+
+            while(i <= aantalsectoren)
+            {
+                Add(i);
+                i++;
+            }
+        }
+
+        public void Add(int sectorNr)
+        {
+            Sector sector = new Sector(sectorNr);
+            sectoren.Add(sector);
+        }
     }
 }
